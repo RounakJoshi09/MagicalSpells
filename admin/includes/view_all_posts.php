888a6b9b10d136643f76320a1c6fb889f27cfb0a
Rounak <rounakjsh783@gmail.com>
2022-03-05 10:25:12 +0530

    Little change in view_all_post

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -14,6 +14,8 @@
               <th>Tags</th>
               <th>Comments</th>
               <th>Date</th>
+              <th>Edit</th>
+              <th>Delete</th>
           </tr>
       </thead>
       <tbody>
