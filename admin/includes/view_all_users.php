a243e925ee3bd06386711e93faa732cd202c6285
Rounak <rounakjsh783@gmail.com>
2022-03-05 11:59:56 +0530

    Users Frontend Table Created+Display User Query Added

---
+++ admin/includes/view_all_users.php
@@ -0,0 +1,73 @@
+
+<table class="table table-bordered table-hover">
+      <thead>
+          <tr>
+              <th>Id</th>
+              <th>Username</th>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Email</th>
+              <th>Date</th>
+              <th>Role</th>
+          </tr>
+      </thead>
+      <tbody>
+
+      <?php 
+
+
+  $query = "SELECT * FROM users";
+  $select_comments = mysqli_query($connection,$query);  
+
+  while($row = mysqli_fetch_assoc($select_comments)) {
+  $user_id = $row['user_id'];
+  $username = $row['username'];
+  $user_firstname = $row['user_firstname'];
+  $user_lastname=$row['user_lastname'];
+  $user_email= $row['user_email'];
+  $user_role = $row['user_role'];
+  $user_registration_date=$row['user_registration_date'];
+   
+
+
+  echo "<tr>";
+      
+  echo "<td>{$user_id}</td>";
+  echo "<td>{$username}</td>";
+  echo "<td>{$user_firstname}</td>";
+  echo "<td>{$user_lastname}</td>";
+  echo "<td>{$user_email}</td>";
+  echo "<td>{$user_registration_date}</td>";
+  echo "<td>{$user_role}</td>";
+//   // Querry to display post from comment_post_id
+//   $query="SELECT * FROM posts WHERE post_id=$comment_post_id";
+//   $comment_query=mysqli_query($connection,$query);
+//   while($row=mysqli_fetch_assoc($comment_query))
+//   {
+//             $post_id=$row['post_id'];
+//             $post_title=$row['post_title'];
+//   }
+//   echo "<td><a href='/cms/post.php?p_id={$post_id}'>{$post_title}</a></td>";
+ 
+//   $query="SELECT * FROM categories WHERE cat_id={$post_category_id}";
+//   $query_categories=mysqli_query($connection,$query);
+//   checkQuery($query);
+//    while($row=mysqli_fetch_assoc($query_categories))
+//    {
+//        $post_category=$row['cat_title'];
+//         echo "<td>{$post_category}</td>"; 
+//    } 
+ 
+  echo "<td><a href='comments.php?approve={$user_id}'>Approve</a></td>";
+   echo "<td><a href='comments.php?unapprove={$user_id}'>Unapprove</a></td>";
+  ?>
+    <td><a href="/cms/admin/comments.php?delete_user=<?php echo $user_id ?>">Delete</a></td>
+  <?php
+  echo "</tr>";
+
+  }
+
+?>
+      </tbody>
+  </table>
+
---
+++ admin/users.php
@@ -0,0 +1,99 @@
+<?php include "includes/admin_header.php" ?>
+    
+    <div id="wrapper">
+        <!-- Navigation -->
+ 
+        <?php include "includes/admin_navigation.php" ?>
+
+
+    <!-- DELETE QUERY -->
+    
+        
+<div id="page-wrapper">
+
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+
+
+            <h1 class="page-header">
+                Welcome to admin
+                <small>Author</small>
+            </h1>
+
+            <div class="col-xs-6">
+            <?php             
+               if(isset($_GET['source']))
+               {
+                    $source=$_GET['source'];
+               }   
+               else
+               {
+                   $source="";
+               }  
+               switch($source)
+               {
+                   case 'add_user';
+                   include "includes/add_user.php";
+                   break;
+                   case 'edit_post';
+                   include "includes/edit_post.php";
+                   break;
+                   case 120;
+                   echo "NICE 120";
+                   break;
+                   default:
+                   include "includes/view_all_users.php";
+                   break;
+               }
+                ?>        
+                </div>        
+
+
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+
+
+<?php 
+    
+       if(isset($_GET['delete_comment']))
+       {
+           $comment_id=$_GET['delete_comment'];
+           $query="DELETE FROM comments WHERE comment_id={$comment_id}";
+           $delete_query=mysqli_query($connection,$query);
+           header("Location:comments.php");
+           checkQuery($delete_query);
+       }
+       if(isset($_GET['approve']))
+       {
+           $comment_id=$_GET['approve'];
+           $query="UPDATE comments SET comment_status='Approved' WHERE comment_id={$comment_id}";
+           $approve_query=mysqli_query($connection,$query);
+           header("Location:comments.php");
+           checkQuery($approve_query);
+       }
+       if(isset($_GET['unapprove']))
+       {
+           $comment_id=$_GET['unapprove'];
+           $query="UPDATE comments SET comment_status='Unapproved' WHERE comment_id={$comment_id}";
+           $unapprove_query=mysqli_query($connection,$query);
+           header("Location:comments.php");
+           checkQuery($unapprove_query);
+       }
+
+    ?>
+    
+  
+     
+        
+        <!-- /#page-wrapper -->
+        
+    <?php include "includes/admin_footer.php" ?>
