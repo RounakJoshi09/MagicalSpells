e70a614a5b7e259f8b28efbc84302f91cd1dbbd4
Rounak <rounakjsh783@gmail.com>
2022-03-09 15:14:45 +0530

    Dashboard almost completed

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,9 +1,43 @@
-<?php ob_start();?> 
-<?php session_start(); ?>
-<?php include "../includes/db.php"?>
+<?php ob_start(); ?>
+<?php include "../includes/db.php"; ?>
 <?php include "functions.php"; ?>
+
+
+
+<?php session_start(); ?>
+
+
+<?php 
+
+
+
+
+if(isset($_SESSION['user_role'])) {
+
+
+
+} else {
+
+header("location: ../index.php");
+
+
+}
+
+
+
+
+ ?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
+
 <head>
 
     <meta charset="utf-8">
@@ -30,6 +64,29 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+ <link href="css/styles.css" rel="stylesheet">
+
+ 
+ <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
+
+
+ <script src="http://tinymce.cachefly.net/4.1/tinymce.min.js"></script>
+
+    <!-- Can use this one below as well -->
+<!--   <script src="http://cdn.tinymce.com/4/tinymce.min.js"></script> -->
+
+
+<script src="js/jquery.js"></script>
+
+
+ 
+
+
+ 
 </head>
 
 <body>
+
+
+
