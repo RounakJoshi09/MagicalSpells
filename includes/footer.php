74b63720ba4f828c7040dfea83f801e901b84a3b
Sakshi Sahu <sakshisahu.3475@gmail.com>
2022-04-06 08:59:18 +0530

    modified includes/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; Our Website 2022</p>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
